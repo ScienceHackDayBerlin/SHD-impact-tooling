0d76400dd97829f6d37d4505a4f2c8f59dfe07f5
action-script <mail.actionscript@gmail.com>
2016-09-24 18:31:06 +0200

    transport loop

--- public/main.js
+++ public/main.js
@@ -1,8 +1,37 @@
 (function() {
+	App = (function() {
+   	function App() {
+			this.numberOfTeamMembers = 0;
+      	this.listen('main');
+    	}
 
-	function work() {
-		console.log('hello');
-	}
+		App.prototype.listen = function(id) {
+			document.getElementById(id).addEventListener( 'click', this.process.bind(this) );
+		};
 
-	document.getElementById("main").addEventListener("click", work);
+    	App.prototype.process = function() {
+			this.checkNumberOfTeamMembers();
+    	};
+
+		App.prototype.checkNumberOfTeamMembers = function() {
+			var number = document.getElementById('number_of_team_members').value;
+
+			if (number != this.numberOfTeamMembers) {
+				var template = document.getElementById('amount_of_km_template').innerHTML;
+				var row = document.getElementById('amount_of_km');
+				var html = '';
+				for (var i = 0; i < number; i++) {
+					html += '<br/>' + template;
+				} 
+				row.innerHTML = html;
+			}
+	
+			this.numberOfTeamMembers = number;
+		};
+       
+    return App;
+    
+	})();
+
+	new App();
 })()
